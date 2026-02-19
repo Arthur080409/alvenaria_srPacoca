09d978f0d08a85314c8537237ffc6b3ae1089841
KaylaneStellaTC <kaylanestella73@gmail.com>
2026-02-18 23:18:49 -0300

    man
    deve ter bastante coisa errada, fiz e refiz, abri pasta  fechei pastas e ainda deve ta tudo errado pq nao entendo mais nada da minha vida. ugh

--- Alvenaria_SrPacoca/screens/Loginscreen.jsx
+++ Alvenaria_SrPacoca/screens/Loginscreen.jsx
@@ -1,12 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import {useState} from 'react';
+export default function login() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [user, setUser] = useState ("");
 
-export default function App() {
+if (!email || !senha || !user){
+  alert("necessario preencher as lacunas.")
+  return;}
+  console.log("usuario:", user);
+    console.log("email:", email);
+    console.log("senha:", senha);
+  }
   return (
     <View style={styles.container}>
-    
-      <Text style={{ color: 'red', fontSize: '20px' }}>bem vindo</Text>
-      <StatusBar style="auto" />
-    </View>
+      <TextInput
+            placeholderr="digite seu nickname"
+            value={user}
+            onChangeText={(e) => setUser(e.target.value)}
+            style={styles.input}
+          />
+
+            <TextInput
+            placeholder="digite seu email"
+            value={email}
+            onChangeText={(e) => setEmail(e.target.value)}
+            style={styles.input}
+          />
+            <TextInput
+            placeholder="digite sua senha"
+            value={senha}
+            onChangeText={(e) => setSenha(e.target.value)}
+            style={styles.input}
+          />
+            <Button type="" style={styles.button}>
+            Entrar
+          </Button>
+          </View>
   );
-}
+          
+
+
+
+  
